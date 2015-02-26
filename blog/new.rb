a9f203823fd1eec9ad3b541c02fdf1c35e819bd1
acarl005 <acarl005@g.ucla.edu>
2015-02-25 21:44:36 -0800

    cleaned up blog posts

--- blog/new.rb
+++ blog/new.rb
@@ -7,6 +7,15 @@ foo.map(&:upcase)
 # => ["I", "LOVE", "TO", "CODE"]
 
 
+upper = Proc.new { |word| word.upcase}
+["i", "love", "to", "code"].map(&upper)
+# => ["I", "LOVE", "TO", "CODE"]
+["got", "asked", "out", "on", "april", "1st"].map(&upper)
+# => ["GOT", "ASKED", "OUT", "ON", "APRIL", "1ST"]
+["my", "armpit", "is", "a", "taco"].map(&upper)
+# => ["MY", "ARMPIT", "IS", "A", "TACO"]
+
+
 class Symbol
   def to_proc
     Proc.new { |obj, *args| obj.send(self, *args) }
@@ -49,20 +58,19 @@ end
 
 
 stuff = [3, "corn", 6, 34, "gorilla", :four]
-
 stuff.select(&[:is_a?, Integer])
 # => [3, 6, 34]
-
 stuff.select(&[:is_a?, String])
 # => ["corn", "gorilla"]
 
-
 letter = "t"
-
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
-
 i_want_pets.map(&:to_s).select(&[:include?, letter])
 # => ["want", "pets", "but"]
+
+group_1 = [4.1, 5.5, 3.2, 3.3, 6.1, 3.9, 4.7]
+group_1.reject(&[:<, 4])
+# => [4.1, 5.5, 6.1, 4.7]
 
 
 
